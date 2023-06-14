34c4b4582b47e6147aeecd2dd925694968e1c5a4
Harshil Pahuja <71945852+harshil162@users.noreply.github.com>
2023-06-14 00:40:58 -0400

    changes

--- my-app/src/searchBar.js
+++ my-app/src/searchBar.js
@@ -2,3 +2,22 @@ import React, {useState} from 'react'
 const searchBar = () => {}
 const [searchInput, setSearchInput] = useState("")
 //Implementation here
+function App(){
+    const [inputText, setInputText] = useState;
+    let inputHandler = (e) => {
+        //convert input text to lower case
+        var lowerCase = e.target.value.toLowerCase();
+        setInputText(lowerCase);
+    };
+    return (
+        <><div className="search">
+            <TextField
+                id="outlined-basic"
+                onChange={inputHandler}
+                variant="outlined"
+                fullWidth
+                label="Search" />
+        </div><List input={inputText} /></>
+    );
+}
+export default App;
