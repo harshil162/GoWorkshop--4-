48fece0b8015277aaa75bcb0ab71e1484d92005d
Harshil Pahuja <71945852+harshil162@users.noreply.github.com>
2023-06-30 23:38:10 -0400

    changes

--- my-app/src/GoWorkshop/key.go
+++
@@ -1,6 +0,0 @@
-package main
-
-//Personal Google API key
-func getKey() string {
-	return "AIzaSyAUZ_4vzQP-2c5bQf7x6bSqGTyO7PVBgeM"
-}
--- my-app/src/GoWorkshop/main.go
+++ my-app/src/GoWorkshop/main.go
@@ -1,28 +1,33 @@
 package main
 
 import (
-	"context"
+	//"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-
 	//"strconv"
 	//"time"
-
 	//"github.com/google/uuid"
 	//"github.com/gorilla/mux"
 	//"golang.org/x/oauth2"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-
-	"google.golang.org/api/option"
-	"google.golang.org/api/sheets/v4"
+	//"golang.org/x/oauth2"
+	//"golang.org/x/oauth2/google"
+	//"google.golang.org/api/option"
+	//"google.golang.org/api/sheets/v4"
 )
 
 var items []Item
+
+type Item struct {
+	Name      string `json:"name"`
+	Artist    string `json:"price"`
+	Year      uint   `json:"year"`
+	Genre     string `json:"genre"`
+	Available bool   `json:"available"`
+}
 
 // Struct for JSON response
 type Response struct {
@@ -32,7 +37,7 @@ type Response struct {
 	Items   []Item `json:"data"`
 }
 
-func CredentialsFromFile(ctx context.Context, credentialsPath string, scopes ...string) (*oauth2.Config, error) {
+/*func CredentialsFromFile(ctx context.Context, credentialsPath string, scopes ...string) (*oauth2.Config, error) {
 	// Read the service account credentials file
 	credentialsFile, err := os.Open(credentialsPath)
 	if err != nil {
@@ -52,7 +57,7 @@ func CredentialsFromFile(ctx context.Context, credentialsPath string, scopes ...
 		return nil, fmt.Errorf("failed to parse credentials: %v", err)
 	}
 	return config, nil
-}
+}*/
 
 /*
 	func NewServer() *Response {
@@ -63,38 +68,22 @@ func CredentialsFromFile(ctx context.Context, credentialsPath string, scopes ...
 		return i
 	}
 */
-func searchSong(songName string) (bool, error) {
-	//Create a context and read the credentials file
-	ctx := context.Background()
-	creds, err := google.FindDefaultCredentials(ctx, sheets.SpreadsheetsScope)
-	if err != nil {
-		return false, err
-	}
-	//Create a new Sheets client using the credentials
-	client, err := sheets.NewService(ctx, option.WithCredentials(creds))
-	if err != nil {
-		return false, err
-	}
-	//Specify the spreadsheet ID and range
-	spreadsheetID := "1Ffs21UxsHPnvwM4l-wHdPxphKcm6usWWZMOyoGx8WjA/edit#gid=0"
-	readRange := "MusicDownloaderSheet!A1:E6"
-	//Make the API call to read the data from the sheet
-	resp, err := client.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
-	if err != nil {
-		return false, err
-	}
-	for _, row := range resp.Values {
-		if len(row) > 0 && row[0] == songName {
-			return true, nil
-		}
-	}
-	return false, nil
-}
-func searchHandler(w http.ResponseWriter, r *http.Request) {
+/*func searchSong(songName string) (bool, error) {
+	// Open Google Sheet using ID
+	var ss = SpreadsheetApp.openById("1dBBPUN-4cXYnNYgshk3m64iotqWo7A3rp1o5rmRR9_CtGMffis5pD0Vh");
+	var sheet = ss.getSheetByName("MusicSheet");
+	// Read all data rows from Google Sheet
+	const values = sheet.getRange(2, 1, sheet.getLastRow() - 1, sheet.getLastColumn()).getValues();
+	// Converts data rows in json format
+	const result = JSON.stringify(values.map(([a,b]) => ({ProductId: a,ProductName:b,})));
+	// Returns Result
+	return ContentService.createTextOutput(result).setMimeType(ContentService.MimeType.JSON);
+}*/
+/*func searchHandler(w http.ResponseWriter, r *http.Request) {
 	// Get the search query from the request
 	query := r.FormValue("search")
 	// Perform the search
-	found, err := searchSong(query)
+	found, err := doGet(query)
 	if err != nil {
 		log.Printf("Search error: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -107,80 +96,39 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "No Results")
 	}
 }
+
+func doGet(query string) {
+	panic("unimplemented")
+}*/
 func main() {
-	// Set up routes
-	http.HandleFunc("/search", searchHandler)
-
-	// Start the server
-	log.Println("Server started on port 3000")
-	//log.Fatal(http.ListenAndServe(":3000", nil))
-
-	/*fileServer := http.FileServer(http.Dir("www/"))
-	http.HandleFunc("/", fileServer.ServeHTTP)
-	http.HandleFunc("/Items", getItemsHandler)
-	log.Println(http.ListenAndServe(":3000", nil))
-	// Replace with the path to your service account credentials JSON file (to-do)
-	credentialsPath := "/path/to/service_account_credentials.json"
-
-	// Read the service account credentials file
-	credentials, err := ioutil.ReadFile(credentialsPath)
+	// Read the returned response
+	url := "https://script.google.com/macros/s/AKfycbzGNTp1p90gbdf1yJPmpNrnh_s5_VhQp7wzpcy4hWYsDIKfMOOYuNzvhzubd6nUWIWiMQ"
+	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
+		log.Fatal(err)
 	}
 
-	// Create a Config from the JSON credentials
-	config, err := google.JWTConfigFromJSON(credentials, sheets.SpreadsheetsScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-
-	// Create a client for the Google Sheets API
-	client := config.Client(context.Background())
-
-	// Replace with the ID of your Google Sheet
-	spreadsheetID := "1Ffs21UxsHPnvwM4l-wHdPxphKcm6usWWZMOyoGx8WjA/edit#gid=0"
-
-	// Replace with the name or range of the sheet you want to retrieve data from
-	sheetName := "MusicDownloaderSheet"
-
-	// Make the API call to retrieve data from the sheet
-	//resp, err := client.Get(fmt.Sprintf("https://docs.google.com/spreadsheets/d/1Ffs21UxsHPnvwM4l-wHdPxphKcm6usWWZMOyoGx8WjA/edit#gid=0", spreadsheetID, sheetName))
-	resp, err := client.Get(fmt.Sprintf("https://sheets.googleapis.com/v4/spreadsheets/%s/values/%s", spreadsheetID, sheetName))
-	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet: %v", err)
-	}
 	defer resp.Body.Close()
 
-	// Read the response body
-	data, err := ioutil.ReadAll(resp.Body)
+	// Read the body of the response
+	htmlData, err := ioutil.ReadAll(resp.Body)
+
 	if err != nil {
-		log.Fatalf("Unable to read response body: %v", err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	// Print the response data
-	fmt.Println(string(data))
+	// print out
+	fmt.Println(string(htmlData)) // The data is returned as []byte, so string required to display it correctly
 
-	// Process the response and print the data
-	/*if len(resp.Values) == 0 {
-		fmt.Println("No data found.")
-	} else if len(resp.Values) > 0 {
-		fmt.Println("Data:")
-		for _, row := range resp.Values {
-			for _, cell := range row {
-				fmt.Printf("%s ", cell)
-			}
-			fmt.Println()
-		}
-	}*/
-	//startUpdateTimer()
-
-	//log.Println(http.ListenAndServe(":"+strconv.Itoa(localhost:3000), nil))
-	//Start the HTTP server on localhost:8080
-	//log.Println("Server is running on port 8080")
+	// Unmarshall the returned []byte into json
+	var items []Item
+	json.Unmarshal([]byte(htmlData), &items)
+	fmt.Printf("id: %v, description: %s", items[0].Available, items[0].Name)
 	log.Println(http.ListenAndServe(":3000", nil))
 }
 
-func jsonResponse(w http.ResponseWriter, x interface{}) {
+/*func jsonResponse(w http.ResponseWriter, x interface{}) {
 	bytes, err := json.Marshal(x)
 	if err != nil {
 		log.Println(err)
@@ -189,11 +137,7 @@ func jsonResponse(w http.ResponseWriter, x interface{}) {
 
 	w.Header().Set("Content-Type", "application/json")
 	_, _ = w.Write(bytes)
-}
-
-func getItemsHandler(w http.ResponseWriter, _ *http.Request) {
-	jsonResponse(w, &items)
-}
+}*/
 
 /*func startUpdateTimer() {
 	ticker := time.NewTicker(2 * time.Second)
